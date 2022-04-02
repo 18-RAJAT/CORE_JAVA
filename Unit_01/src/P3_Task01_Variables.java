7e5ebfd8c5376b1a859fd7a411c4b7a225a60ac9
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-04-02 19:00:48 +0530

    first commit

---
+++ Unit_01/src/P3_Task01_Variables.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class P3_Task01_Variables {
+    public static void main(String args[])
+    {
+        String name="Alex Jersey";
+        int age=27;
+        long salary=1000000;
+        float tax=300.355f;
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+        System.out.println("Salary : "+salary);
+        System.out.println("Tax : "+tax);
+    }
+}
---
+++ Unit_01/src/P3_Task02_TypeConversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class P3_Task02_TypeConversion {
+    public static void main(String args[])
+    {
+        int i=97;
+        double x=i;
+        System.out.println((char)i);
+        System.out.println(x);
+    }
+}
