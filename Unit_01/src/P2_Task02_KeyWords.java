11855da7a08dc1481ce7ade8030b3ea2d1ba66eb
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-04-12 11:54:32 +0530

    first commit

--- Unit_01/src/P2_Task02_KeyWords.java
+++ Unit_01/src/P2_Task02_KeyWords.java
@@ -1,22 +1,22 @@
-public class P2_Task02_KeyWords {   public static void main(String[] args) {
-    //Identifier expected
-       int double = 1.5f;
-    //Not a statement
-       int("hello");
-    }
-    //Identifier expected
-    int int(string x)
-    {
-    //Cannot resolve method 'println(string)
-        System.out.println(x);
-    //Cannot return a value from a method with void result type
-        return 0;
-    }
-    //Invalid method declaration; return type required
-    SampleClass boolean = new SampleClass();
-}
-
-class SampleClass
-{
-    int a;
-}
+//public class P2_Task02_KeyWords {   public static void main(String[] args) {
+//    //Identifier expected
+//       int double = 1.5f;
+//    //Not a statement
+//       int("hello");
+//    }
+//    //Identifier expected
+//    int int(string x)
+//    {
+//    //Cannot resolve method 'println(string)
+//        System.out.println(x);
+//    //Cannot return a value from a method with void result type
+//        return 0;
+//    }
+//    //Invalid method declaration; return type required
+//    SampleClass boolean = new SampleClass();
+//}
+//
+//class SampleClass
+//{
+//    int a;
+//}
