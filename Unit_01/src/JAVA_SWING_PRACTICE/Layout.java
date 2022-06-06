898fd42c49a357be2eddb71cee898eb9dd0a70a0
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-06-06 13:55:48 +0530

    first commit

--- Unit_01/src/JAVA_SWING_PRACTICE/Layout.java
+++ Unit_01/src/JAVA_SWING_PRACTICE/Layout.java
@@ -1,4 +1,24 @@
 package JAVA_SWING_PRACTICE;
 
+import javax.swing.*;
+
 public class Layout {
+    public static void main(String[]args)
+    {
+            first obj=new first();
+    }
+}
+class first extends JFrame
+{
+    public first()
+    {
+        JLabel l=new JLabel("Hello");
+        JLabel ll=new JLabel("World");
+
+        add(l);
+        add(ll);
+
+        setVisible(true);
+        setSize(400,400);
+    }
 }
