48fafe4730fbaf69e3f0561240b212f74730a58c
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-06-06 14:26:51 +0530

    first commit

---
+++ Unit_01/src/JAVA_SWING_PRACTICE/Add_Two_Numbers.java
@@ -0,0 +1,46 @@
+package JAVA_SWING_PRACTICE;
+
+import javax.swing.*;
+import java.awt.*;//Flowlayout
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Add_Two_Numbers {
+    public static void main(String[]args)
+    {
+        Addition obj=new Addition();
+    }
+}
+class Addition extends JFrame implements ActionListener
+{
+    JTextField t1,t2;
+    JButton b;
+    JLabel l;
+    public Addition()
+    {
+         t1=new JTextField(20);
+         t2=new JTextField(20);
+         b=new JButton("OK");
+         l=new JLabel("RESULT");
+
+        add(t1);
+        add(t2);
+        add(b);
+        add(l);
+
+        b.addActionListener(this);//It is an interface
+        setLayout(new FlowLayout());
+        setVisible(true);
+        setSize(400,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public void actionPerformed(ActionEvent ae)
+    {
+        int num1=Integer.parseInt(t1.getText());
+        int num2=Integer.parseInt(t2.getText());
+
+        int addition=num1+num2;
+        l.setText(addition+" ");
+
+    }
+}
