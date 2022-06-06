3b7223633b05a510de90d1b38b2530d6f6128185
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-06-06 14:02:00 +0530

    first commit

--- Unit_01/src/JAVA_SWING_PRACTICE/Layout.java
+++ Unit_01/src/JAVA_SWING_PRACTICE/Layout.java
@@ -1,6 +1,7 @@
 package JAVA_SWING_PRACTICE;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Layout {
     public static void main(String[]args)
@@ -12,6 +13,9 @@ class first extends JFrame
 {
     public first()
     {
+        //center of JFrame and combine them
+        setLayout(new FlowLayout());//Flowlayout,Grid,null layouts...!!
+
         JLabel l=new JLabel("Hello");
         JLabel ll=new JLabel("World");
 
@@ -20,5 +24,6 @@ class first extends JFrame
 
         setVisible(true);
         setSize(400,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
